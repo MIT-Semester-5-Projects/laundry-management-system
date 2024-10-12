5310ae9d20c40cb2280503d09507a4dae130cd65
Rish <2004rish@gmail.com>
2024-10-12 15:14:01 +0530

    feat: touching up navbar component

---
+++ web_client/src/lib/components/footer.svelte
@@ -0,0 +1,9 @@
+<script>
+</script>
+
+<footer
+	class="from-myblue to-mybrown absolute bottom-0 flex w-full justify-between bg-gradient-to-r from-0% via-blue-800 via-35% to-40% text-sm text-white"
+>
+	<span>@2024 Quick Smart Wash</span>
+	<span>Caring For Second Skin</span>
+</footer>
--- web_client/src/lib/components/navbar.svelte
+++ web_client/src/lib/components/navbar.svelte
@@ -1,46 +1,12 @@
-<script lang="ts">
-	let isContactClicked = false;
-	let isSocialsClicked = false;
-	const handlecontactclick = () => {
-		isContactClicked = !isContactClicked;
-	};
-	const handlesocialsclick = () => {
-		isSocialsClicked = !isSocialsClicked;
-	};
-	const redirect = () => {
-		location.href = '/services';
-	};
+<script>
+	// your script goes here
+	import Navlist from './navlist.svelte';
 </script>
 
-<nav
-	class="from-myblue to-mybrown flex h-[15vh] w-full items-end justify-between bg-gradient-to-r from-5% to-100% pt-2"
->
-	<a href="/" class="pl-2">
-		<img class="mt-2 w-[75%]" src="/logo.png" alt="Laundry Business Logo" />
-	</a>
-	<ul class="m-0 flex list-none pb-5 pl-5 pr-5 text-white">
-		<li class="pr-8">
-			<button
-				type="button"
-				class="text-2xl hover:text-blue-500"
-				on:click={redirect}
-				on:keydown={redirect}>Services</button
-			>
-		</li>
-		<li class="pr-8">
-			<button type="button" class="text-2xl hover:text-blue-500" on:click={handlecontactclick}>
-				Contact Us
-			</button>
-		</li>
-		<li class="pr-8 hover:text-blue-500">
-			<button
-				type="button"
-				class="text-2xl hover:text-blue-500"
-				on:keydown={handlecontactclick}
-				on:click={handlecontactclick}
-			>
-				Socials
-			</button>
-		</li>
-	</ul>
+<nav class="from-myblue to-mybrown flex items-end justify-between bg-gradient-to-r from-5% to-100%">
+	<a href="/"><img class="h-16 duration-500 ease-in hover:h-24" src="/logo.png" alt="" /></a>
+	<div class="">
+		<Navlist />
+	</div>
+	<slot />
 </nav>
