07e5e437d41655487ece0f02a2d10da22efa171d
nnniv <nivroh2016@gmail.com>
2024-10-26 19:42:29 +0530

    feat: connect to db

---
+++ server/db.py
@@ -0,0 +1,35 @@
+import mariadb
+import os
+import dotenv
+
+dotenv.load_dotenv()
+
+DB_USER = os.environ.get("DB_USER")
+DB_PASS = os.environ.get("DB_PASS")
+# Database connection parameters
+db_config = {
+    "host": "127.0.0.1",
+    "port": 3353,
+    "user": DB_USER,
+    "password": DB_PASS,
+    "database": "laundry_management",
+}
+
+try:
+    # Establish connection
+    conn = mariadb.connect(**db_config)
+    print("Connection to the database established successfully!")
+
+    # Example operation
+    cursor = conn.cursor()
+    cursor.execute("SHOW TABLES;")
+    for table in cursor:
+        print(table)
+
+except mariadb.Error as e:
+    print(f"Error connecting to MariaDB: {e}")
+
+finally:
+    # Close the connection
+    if conn:
+        conn.close()
