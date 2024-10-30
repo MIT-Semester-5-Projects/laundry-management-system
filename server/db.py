43fa808e2da44892a6b51f268c37d7d948c81e48
nikhil <nikh.nam04@gmail.com>
2024-10-30 20:27:37 +0530

    chore: cleaned connection code

--- server/db.py
+++ server/db.py
@@ -1,8 +1,6 @@
 import mariadb
-import mysql
 import os
 import dotenv
-
 
 dotenv.load_dotenv()
 
@@ -11,21 +9,18 @@ DB_PASS = os.environ.get("DB_PASS")
 # Database connection parameters
 db_config = {
     "host": "127.0.0.1",
-    "port": 3353,
+    "port": 3353,   # change it to 3353 for Linux, 3306 for Windows
     "user": DB_USER,
     "password": DB_PASS,
     "database": "laundry_management",
 }
 
-try:
-    # Establish connection
-    if os.name == "posix":
-        conn = mariadb.connect(**db_config)
-        print("Connection to the database established successfully!")
-    else:
-        conn = mysql.connector.connect(**db_config)
-        print("Connection to the database established successfully!")
+conn = None #Initialize variable
 
+try:
+    #Establish connection
+    conn = mariadb.connect(**db_config)
+    print("Connection to the database established successfully!")
 
     # Example operation
     cursor = conn.cursor()
